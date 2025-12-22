391028112db1e9249f83e829de2bc53c73758797
saulocapistrano <saulocapistrano@gmail.com>
2025-12-22 16:14:11 -0300

    Initialize Maven project with Spring Boot setup, including wrapper scripts, POM configuration, and basic application structure.

---
+++ src/main/java/br/com/analise/creditoanaliseworker/CreditoAnaliseWorkerApplication.java
@@ -0,0 +1,13 @@
+package br.com.analise.creditoanaliseworker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CreditoAnaliseWorkerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CreditoAnaliseWorkerApplication.class, args);
+	}
+
+}
