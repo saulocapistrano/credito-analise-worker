4a0ae82de717b232738a03aa3e60d571b471f026
saulocapistrano <saulocapistrano@gmail.com>
2025-12-22 16:14:47 -0300

    Format code in CreditoAnaliseWorkerApplication for improved readability by adjusting indentation in the main method.

--- src/main/java/br/com/analise/creditoanaliseworker/CreditoAnaliseWorkerApplication.java
+++ src/main/java/br/com/analise/creditoanaliseworker/CreditoAnaliseWorkerApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class CreditoAnaliseWorkerApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(CreditoAnaliseWorkerApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(CreditoAnaliseWorkerApplication.class, args);
+    }
 
 }
